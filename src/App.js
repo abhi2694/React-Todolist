ffbba740b9c0b76c8b2ca50eb91ce793f0920cf2
Abhinav Kumar <abhinav@legalcopy.com>
2021-05-15 16:00:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodolistMain from './todolist.js'
+
+function App() {
+  return (
+    <div id="root">
+      <TodolistMain></TodolistMain>
+    </div>
+  );
+}
+
+export default App;
